5c05174f9c61a74f6174d354c7292d5239c2251d
Dima-tyman <tyman-dima@list.ru>
2023-06-24 13:52:52 +0300

    Add view module v1.0. Add Notepad class v1.0

---
+++ Notepad.py
@@ -0,0 +1,31 @@
+import datetime
+
+
+class Notepad:
+
+    def __init__(self, name, content=""):
+        date_init = datetime.datetime.now()
+        self.id = date_init.__hash__()
+        self.date_create = date_init
+        self.date_modify = date_init
+
+        if name == "":
+            self.name = "Note" + str(self.id)
+        else:
+            self.name = name
+
+        self.content = content
+
+    def __str__(self):
+        return self.name + ' (' + str(self.date_create).split('.')[0] + ')'
+
+    def modify(self, content):
+        self.content = content
+        self.date_modify = datetime.datetime.now().date()
+
+    def get_dictionary(self):
+        return {"id": self.id,
+                "name": self.name,
+                "content": self.content,
+                "date_create": self.date_create,
+                "date_modify": self.date_modify}
---
+++ view.py
@@ -0,0 +1,38 @@
+POSSIBLE_FILTER = ["name", "create", "modify"]
+
+
+def show_notes(notes_list, filter_tag=POSSIBLE_FILTER[1]):
+    if not isinstance(notes_list, list):
+        print("ERROR(not array)")
+    else:
+        if filter_tag not in POSSIBLE_FILTER:
+            print("Non available filter! Filter list: " + ', '.join(POSSIBLE_FILTER) + '.')
+        else:
+            print("NOTE LIST:", end='\n\t')
+            if filter_tag == POSSIBLE_FILTER[0]:
+                print(*sorted(notes_list, key=lambda note: note.name), sep='\n\t')
+            elif filter_tag == POSSIBLE_FILTER[1]:
+                print(*sorted(notes_list, key=lambda note: note.date_create), sep='\n\t')
+            elif filter_tag == POSSIBLE_FILTER[2]:
+                print(*sorted(notes_list, key=lambda note: note.date_modify), sep='\n\t')
+
+
+def show_note(note_dict):
+    if not isinstance(note_dict, dict):
+        print("ERROR(not dictionary)")
+    else:
+        print("NAME\n\t" + note_dict.get("name"))
+        print("CONTENT\n\t" + note_dict.get("content"))
+
+
+def show_note_info(note_dict):
+    if not isinstance(note_dict, dict):
+        print("ERROR(not dictionary)")
+    else:
+        tad_len = 15
+        print("NOTE INFO")
+        print("NAME: ".rjust(tad_len) + note_dict.get("name"))
+        print("ID: ".rjust(tad_len) + str(note_dict.get("id")))
+        print("DATE CREATE: ".rjust(tad_len) + str(note_dict.get("date_create")))
+        print("DATE MODIFY: ".rjust(tad_len) + str(note_dict.get("date_modify")))
+        print("SIZE: ".rjust(tad_len) + str(len(note_dict.get("content"))))
